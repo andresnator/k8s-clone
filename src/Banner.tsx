0c651ad88c374965ffd52920e409a73a873cebd4
Andresnator <andresnator@gmail.com>
2025-12-06 12:48:25 +0100

    feat: update banner description to "Kubernetes Resource Clone Tool (Sandbox environment)"

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -11,7 +11,7 @@ export const Banner = ({ version }: BannerProps) => (
         <Gradient name="teen">
             <BigText text="K8s Clone" />
         </Gradient>
-        <Text color="yellow">Kubernetes Resource Migration Tool</Text>
+        <Text color="yellow">Kubernetes Resource Clone Tool (Sandbox environment)</Text>
         {version && <Text color="gray">Version: v{version}</Text>}
         <Text> </Text>
     </>
